4b37aea8027392a63ce15ec88cdfe6f653094ab4
bairi56 <zubair413427@gmail.com>
2025-05-28 17:20:27 +0500

    changes

--- server.js
+++ server.js
@@ -113,4 +113,6 @@ app.use(adminBro.options.rootPath, router);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`FreshMart Admin running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
